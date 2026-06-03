56cbb0d8aa649c15ce63d2d5576d65d821819132
Agus Heryanto <agusheryantodev@gmail.com>
2026-06-03 23:56:53 +0700

    feat: set up go rest api with clean arc

---
+++ experiments/engine/golang/ecommerce-monolith/internal/controller/restapi/v1/router.go
@@ -0,0 +1,30 @@
+package v1
+
+import (
+	"github.com/agusheryanto182/ecommerce-monolith/internal/controller/restapi/middleware"
+	"github.com/agusheryanto182/ecommerce-monolith/internal/usecase"
+	"github.com/agusheryanto182/ecommerce-monolith/pkg/jwt"
+	"github.com/agusheryanto182/ecommerce-monolith/pkg/logger"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
+
+// NewRoutes -.
+func NewRoutes(apiV1Group fiber.Router, u usecase.User, jwtManager *jwt.Manager, l logger.Interface) {
+	r := &V1{u: u, l: l, v: validator.New(validator.WithRequiredStructEnabled())}
+
+	// Public routes
+	authGroup := apiV1Group.Group("/auth")
+	{
+		authGroup.Post("/register", r.register)
+		authGroup.Post("/login", r.login)
+	}
+
+	// Protected routes
+	protected := apiV1Group.Group("", middleware.Auth(jwtManager))
+
+	userGroup := protected.Group("/user")
+	{
+		userGroup.Get("/profile", r.profile)
+	}
+}
