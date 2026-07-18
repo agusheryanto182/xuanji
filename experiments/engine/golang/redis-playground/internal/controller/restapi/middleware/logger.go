566e4376f92e802f5da97445b534895739f3c1c1
Agus Heryanto <agusheryantodev@gmail.com>
2026-07-19 00:23:57 +0700

    feat: add duration log

--- experiments/engine/golang/redis-playground/internal/controller/restapi/middleware/logger.go
+++ experiments/engine/golang/redis-playground/internal/controller/restapi/middleware/logger.go
@@ -1,12 +1,33 @@
 package middleware
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/agusheryanto182/redis-playground/pkg/logger"
 	"github.com/gofiber/fiber/v2"
 )
+
+func formatBytes(size int) string {
+	const (
+		KB = 1024
+		MB = 1024 * KB
+		GB = 1024 * MB
+	)
+
+	switch {
+	case size >= GB:
+		return fmt.Sprintf("%.2f GB", float64(size)/GB)
+	case size >= MB:
+		return fmt.Sprintf("%.2f MB", float64(size)/MB)
+	case size >= KB:
+		return fmt.Sprintf("%.2f KB", float64(size)/KB)
+	default:
+		return fmt.Sprintf("%d B", size)
+	}
+}
 
 func buildRequestMessage(ctx *fiber.Ctx) string {
 	var result strings.Builder
@@ -19,16 +40,22 @@ func buildRequestMessage(ctx *fiber.Ctx) string {
 	result.WriteString(" - ")
 	result.WriteString(strconv.Itoa(ctx.Response().StatusCode()))
 	result.WriteString(" ")
-	result.WriteString(strconv.Itoa(len(ctx.Response().Body())))
+	result.WriteString(formatBytes(len(ctx.Response().Body())))
 
 	return result.String()
 }
 
-func Logger(l logger.Interface) func(c *fiber.Ctx) error {
+func Logger(l logger.Interface) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
+		start := time.Now()
+
 		err := ctx.Next()
 
-		l.Info("%s", buildRequestMessage(ctx))
+		l.Info(
+			"%s - duration=%s",
+			buildRequestMessage(ctx),
+			time.Since(start),
+		)
 
 		return err
 	}
